17573ab1f1d11ae9450a858a87c0a405d522f198
maicons89 <maicon.santos89@hotmail.com>
2022-10-26 22:26:02 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'projeto_vinhos_frontend';
+
+  constructor(@Inject(DOCUMENT) document: Document) {}
+
+  changeStyle(color: string, img: string) {
+    (document.querySelector('.circle') as HTMLElement).style.backgroundColor =
+      color;
+
+    (
+      document.querySelector('.corderoconpieldeloborose') as HTMLElement
+    ).setAttribute('src', img);
+  }
 }
